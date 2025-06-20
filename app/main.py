6f461548d0cddb0b7f45733a010b76f514bf4098
Elijah Kimani <elijahkimani1293@gmail.com>
2025-06-20 18:22:37 +0300

    Remove .env from git tracking

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://ecommerce-frontend-blush-two.vercel.app"],
+    allow_origins=["http://localhost:5173", "https://ecommerce-frontend-blush-two.vercel.app","https://www.smartindoordecors.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
